fb3051f9650f309d1706acd106791943007e4e14
Jhonata02 <jhonata.pereira.andrade@ccc.ufcg.edu.br>
2025-05-21 20:18:27 -0300

    GET game by id

---
+++ src/main/java/com/example/dslist/dto/GameDTO.java
@@ -0,0 +1,67 @@
+package com.example.dslist.dto;
+
+import com.example.dslist.entities.Game;
+
+public class GameDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDTO() {
+    }
+
+    public GameDTO(Game game) {
+        this.id = game.getId();
+        this.title = game.getTitle();
+        this.year = game.getYear();
+        this.genre = game.getGenre();
+        this.platforms = game.getPlatforms();
+        this.score = game.getScore();
+        this.imgUrl = game.getImgUrl();
+        this.shortDescription = game.getShortDescription();
+        this.longDescription = game.getLongDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+}
--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -1,13 +1,14 @@
 package com.example.dslist.services;
 
+import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.entities.Game;
 import com.example.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class GameService {
@@ -15,9 +16,16 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> games = gameRepository.findAll();
 
         return games.stream().map(game -> new GameMinDTO(game)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game game = gameRepository.findById(id).get();
+        return new GameDTO(game);
     }
 }
